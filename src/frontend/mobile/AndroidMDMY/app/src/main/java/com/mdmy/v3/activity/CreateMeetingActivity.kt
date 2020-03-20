09d939f80cac54a3058900a7d3ab0cbedd609374
Jeldo <astrokwak@gmail.com>
2020-03-20 20:42:16 +0900

    Issue#29 Modify CreateMeetingActivity

--- src/frontend/mobile/AndroidMDMY/app/src/main/java/com/mdmy/v3/activity/CreateMeetingActivity.kt
+++ src/frontend/mobile/AndroidMDMY/app/src/main/java/com/mdmy/v3/activity/CreateMeetingActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.View.VISIBLE
+import android.view.View.GONE
 import androidx.appcompat.app.AppCompatActivity
 import com.apollographql.apollo.rx2.rxMutate
 import com.mdmy.v3.CreateMeetingMutation
@@ -20,38 +21,48 @@ class CreateMeetingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_meeting)
-
         initViews()
     }
 
-    private fun initViews() {
-        btn_makeroom.setOnClickListener {
-            if (et_meeting_name.text.toString() == "") {
-                tv_meeting_name_err.visibility = VISIBLE
-                return@setOnClickListener
-            }
-            meetingName = et_meeting_name.text.toString()
-            numberOfParticipants = et_participants_num.text.toString().toIntOrNull()
-            if (numberOfParticipants == null || numberOfParticipants!! < 2) {
-                tv_participants_num_err.visibility = VISIBLE
-                return@setOnClickListener
-            }
-            val createMeetingMutation: CreateMeetingMutation =
-                CreateMeetingMutation.builder().meetingName(meetingName)
-                    .numberOfParticipants(numberOfParticipants!!)
-                    .build()
+    private fun isValidForm(): Boolean {
+        var isValid = true
+        meetingName = et_meeting_name.text.toString()
+        if (meetingName.isEmpty()) {
+            tv_meeting_name_err.visibility = VISIBLE
+            isValid = false
+        } else {
+            tv_meeting_name_err.visibility = GONE
+        }
+        numberOfParticipants = et_number_of_participant.text.toString().toIntOrNull()
+        if (numberOfParticipants == null || numberOfParticipants!! < 2) {
+            tv_number_of_participant_err.visibility = VISIBLE
+            isValid = false
+        } else {
+            tv_number_of_participant_err.visibility = GONE
+        }
+        return isValid
+    }
 
-            apolloClient.rxMutate(createMeetingMutation)
+    private fun initViews() {
+        btn_create_meeting.setOnClickListener {
+            if (isValidForm()) {
+                val createMeetingMutation: CreateMeetingMutation =
+                    CreateMeetingMutation.builder().meetingName(meetingName)
+                        .numberOfParticipants(numberOfParticipants!!)
+                        .build()
+
+                apolloClient.rxMutate(createMeetingMutation)
                     //TODO(YoungHwan): Add error handling code
-                .subscribeOn(Schedulers.io()).subscribe(
-                    {
-                        Log.e("RES", it.data().toString())
-                        val intent = Intent(this, MapActivity::class.java)
-                        startActivity(intent)
-                    }, {
-                        Log.e("ERR", it.message.toString())
-                    }
-                )
+                    .subscribeOn(Schedulers.io()).subscribe(
+                        {
+                            Log.e("RES", it.data().toString())
+                            val intent = Intent(this, MapActivity::class.java)
+                            startActivity(intent)
+                        }, {
+                            Log.e("ERR", it.message.toString())
+                        }
+                    )
+            }
         }
     }
 }
