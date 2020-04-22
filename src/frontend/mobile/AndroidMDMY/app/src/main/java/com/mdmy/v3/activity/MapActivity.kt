dde9c2771355104e425b547bef05d92cf2988d62
younghwan <show2881@gmail.com>
2020-04-22 16:17:51 +0900

    add BottomSheet layout
    add recyclerView
    
    add auto complete

--- src/frontend/mobile/AndroidMDMY/app/src/main/java/com/mdmy/v3/activity/MapActivity.kt
+++ src/frontend/mobile/AndroidMDMY/app/src/main/java/com/mdmy/v3/activity/MapActivity.kt
@@ -1,44 +1,128 @@
 package com.mdmy.v3.activity
 
+import android.location.Address
+import android.location.Geocoder
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.view.inputmethod.EditorInfo
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
-import com.mdmy.v3.R
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
-
-
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.mdmy.v3.R
+import com.mdmy.v3.adapter.PlaceResultAdapter
+import com.mdmy.v3.network.PlaceApi
+import kotlinx.android.synthetic.main.activity_map.*
+import kotlinx.android.synthetic.main.bottom_sheet.*
+import kotlinx.coroutines.launch
 
 class MapActivity : AppCompatActivity(), OnMapReadyCallback {
+    private lateinit var mResultList: RecyclerView
+    private lateinit var mResultAdapter: PlaceResultAdapter
+    private lateinit var mMap: GoogleMap
+    private var mPlaceKeyword: String = ""
 
-
-    private lateinit var mMap : GoogleMap
-
-    override fun onCreate(savedInstanceState: Bundle?){
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map)
+        initViews()
+    }
+
+    private fun initViews() {
+        BottomSheetBehavior.STATE_EXPANDED
+        val sheetBehavior = BottomSheetBehavior.from(bottomSheet)
 
         val mapFragment = supportFragmentManager
-            .findFragmentById(R.id.map) as SupportMapFragment?
+            .findFragmentById(R.id.layout_map) as SupportMapFragment?
         mapFragment!!.getMapAsync(this)
+
+        btn_start_location.setOnClickListener {
+            appbar.visibility = View.VISIBLE
+            layout_start_location.visibility = View.INVISIBLE
+            sheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
+        }
+
+        findViewById<EditText>(R.id.et_search_box).run {
+            setOnEditorActionListener { _, action, _ ->
+                when (action) {
+                    EditorInfo.IME_ACTION_SEARCH -> {
+                        searchPlaces()
+                        true
+                    }
+                    else -> false
+                }
+            }
+
+            addTextChangedListener(object : TextWatcher {
+                override fun afterTextChanged(p0: Editable?) {
+                    mPlaceKeyword = p0?.toString() ?: ""
+                }
+                override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                }
+                override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                }
+            })
+        }
+
+        btn_serach.setOnClickListener {
+            mPlaceKeyword = et_search_box.text.toString()
+            searchPlaces()
+        }
+    }
+
+    private fun searchPlaces() {
+        if (mPlaceKeyword.isNotBlank()) {
+            lifecycleScope.launch {
+                val result = PlaceApi.autoComplete(mPlaceKeyword)
+                mResultAdapter.setResult(result.filterNotNull())
+            }
+        }
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        mResultAdapter =
+            PlaceResultAdapter(object : PlaceResultAdapter.ResultClickListener {
+                override fun resultClicked(result: String) {
+                    val point: LatLng = getLatLngFromAddress(result)!!
+                    val mOptions2 = MarkerOptions()
+                    mOptions2.title("search result")
+                    mOptions2.snippet(result)
+                    mOptions2.position(point)
+                    mMap.addMarker(mOptions2)
+                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(point, 15f))
+                }
+            })
+        mResultList = findViewById<RecyclerView>(R.id.rv_list_item).apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = mResultAdapter
+        }
+    }
 
-
-        val mMap = googleMap
-
-        val SEOUL = LatLng(37.56, 126.97)
-
-        val markerOptions = MarkerOptions()
-        markerOptions.position(SEOUL)
-        markerOptions.title("서울")
-        markerOptions.snippet("한국의 수도")
-        mMap.addMarker(markerOptions)
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(SEOUL))
-        mMap.animateCamera(CameraUpdateFactory.zoomTo(10f))
+    private fun getLatLngFromAddress(address: String): LatLng? {
+        val geocoder = Geocoder(this@MapActivity)
+        val addressList: List<Address>?
+        return try {
+            addressList = geocoder.getFromLocationName(address, 1)
+            if (addressList != null) {
+                val singleaddress = addressList[0]
+                LatLng(singleaddress.latitude, singleaddress.longitude)
+            } else {
+                null
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+            null
+        }
     }
 }
---
+++ src/frontend/mobile/AndroidMDMY/app/src/main/java/com/mdmy/v3/adapter/PlaceResultAdapter.kt
@@ -0,0 +1,45 @@
+package com.mdmy.v3.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.mdmy.v3.R
+import com.mdmy.v3.viewholder.PlaceResultViewHolder
+
+class PlaceResultAdapter(private val resultClickListener: ResultClickListener) :
+    RecyclerView.Adapter<PlaceResultViewHolder>() {
+    private val mResultList = mutableListOf<String>()
+
+    interface ResultClickListener {
+        fun resultClicked(result: String)
+    }
+
+    fun setResult(resultList: List<String>) {
+        synchronized(mResultList) {
+            mResultList.clear()
+            mResultList.addAll(resultList)
+        }
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PlaceResultViewHolder {
+        return PlaceResultViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.map_rv_item, parent, false)
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return mResultList.size
+    }
+
+    override fun onBindViewHolder(holder: PlaceResultViewHolder, position: Int) {
+        (holder as? PlaceResultViewHolder)?.run {
+            val result = mResultList[position]
+            update(result)
+            itemView.setOnClickListener {
+                resultClickListener.resultClicked(result)
+            }
+        }
+    }
+}
+
